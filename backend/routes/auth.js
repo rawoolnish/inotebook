8c700e83907e99a952e8330b697d672379a9627c
Nishant <rawoolnishant@gmail.com>
2022-05-07 18:54:03 +0530

    error handling

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,28 +3,38 @@ const router = express.Router();
 require('../db')
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const { json } = require('express');
 
-//create a new user dosent require auth
+//create a new user using : POST "/api/auth/createuser". dosent require login
 
-router.post('/', [
+router.post('/createuser', [
     body('name','Enter a valid name').isLength({ min: 5 }),
     body('email','Enter a valid mail').isEmail(),
     body('password','Enter a valid passkey').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
+    //if there are errors ,return Bad requests and the errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user))
-        .catch(err => {
-            console.log(err)
-            res.json({ error: 'please enter a unique user' ,message:err.message})
-        })
-    
+    //Check weather the user with this email exists already
+    try {
+        
+   
+    let user =await User.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });  
+    res.send(user)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured!")
+    }
     })
 
 
