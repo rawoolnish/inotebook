bd198e88688d324e811cbe0d1689be9f9b6b0fef
Nishant <rawoolnishant@gmail.com>
2022-05-10 12:54:20 +0530

    after token

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,7 +11,8 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+     
+        
     },
     password: {
         type: String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,11 @@ require('../db')
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const { json } = require('express');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+
+const JWT_SECRET = 'Nishantisagoodboy';
 
 //create a new user using : POST "/api/auth/createuser". dosent require login
 
@@ -24,13 +29,25 @@ router.post('/createuser', [
     let user =await User.findOne({ email: req.body.email });
     if (user) {
         return res.status(400).json({ error: "Sorry a user with this email already exists" })
-    }
+        }
+        const salt =await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt) 
+        //create new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         });  
-    res.send(user)
+        const data = {
+            user: {
+                id:user.id
+            }
+        }
+         
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.send({ authtoken })
+     
+        
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured!")
