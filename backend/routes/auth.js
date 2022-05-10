c77fe5034452440d91e6b65222161a53205778db
Nishant <rawoolnishant@gmail.com>
2022-05-10 13:37:38 +0530

    login route finished

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,7 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'Nishantisagoodboy';
 
-//create a new user using : POST "/api/auth/createuser". dosent require login
+//ROUTE:1.create a new user using : POST "/api/auth/createuser". dosent require login
 
 router.post('/createuser', [
     body('name','Enter a valid name').isLength({ min: 5 }),
@@ -54,5 +54,38 @@ router.post('/createuser', [
     }
     })
 
+//ROUTE:2.authenticate user using :POST "api/auth/login".no log in required.
+router.post('/login', [
+    body('email', 'Enter a valid mail').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+//if errors occured return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array() })
+    }
+    const { email, password } = req.body;
+    try {
+        let user =await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Invalid Credentials!" });
+        }
+        const passwordCompare =await  bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Invalid Credentials!" });
+        }
+        const data = {
+            user: {
+        id:user.id
+    }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured!")
+    }
+});
 
 module.exports=router
