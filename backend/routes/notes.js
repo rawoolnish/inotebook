5827895ed158a9903514e428c2580e2a31eb691e
Nishant <rawoolnishant@gmail.com>
2022-05-13 23:32:51 +0530

    react dom done

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -46,21 +46,49 @@ router.post('/addnote', fetchuser, [
 //ROUTE:3, update an existing note using :POST "api/auth/updatenote". login required 
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
     const { title, description, tag } = req.body;
+    try{
     //create a new note
     const newNote = {};
     if (title) { newNote.title = title };
     if (description) { newNote.description = description };
     if (tag) { newNote.tag = tag };
-    
+
     //find the note to be updated and update it
-    let note =await  Note.findById(req.params.id)
-    if (!note) {return res.status(404).send("Not Found!") }
-    
+    let note = await Note.findById(req.params.id)
+    if (!note) { return res.status(404).send("Not Found!") }
+
     if (note.user.toString() !== req.user.id) {
         return res.status(401).send("Not allowed!")
     }
     note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-    res.json({ note });
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured!")
+    }
+
+});
+
+
+
+//ROUTE:4, delete an existing note using :DELETE "api/auth/deletenote". login required 
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+   try {
+    //find the note to be deleted and delete it
+    let note = await Note.findById(req.params.id)
+    if (!note) { return res.status(404).send("Not Found!") }
+
+    //allow deletion only if user owns this note
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not allowed!")
+    }
+    note = await Note.findByIdAndDelete(req.params.id)
+       res.json({ "Success": "Note has been deleted", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured!")
+    }
+
 });
 
 module.exports = router;
